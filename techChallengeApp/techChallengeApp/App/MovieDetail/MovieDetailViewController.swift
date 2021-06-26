eb574d9cf1028354bb9d289a53e5bcc14e2254aa
Unknown <rml7@academy.cin.ufpe.br>
2021-06-26 00:12:01 -0300

    MovieDetal integrated with API

--- techChallengeApp/techChallengeApp/App/MovieDetail/MovieDetailViewController.swift
+++ techChallengeApp/techChallengeApp/App/MovieDetail/MovieDetailViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class MovieDetailViewController: UIViewController {
     
+    init(url:String, title: String, year: String, overview: String) {
+        super.init(nibName: "view", bundle: nil)
+        screen.movieLabel.text = title
+        screen.movieImage.loadImageUsingCache(withUrl: url)
+        screen.yearLabel.text = year
+        screen.overviewLabel.text = overview
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     private let screen = MovieDetailScreen()
     
     override func loadView() {
