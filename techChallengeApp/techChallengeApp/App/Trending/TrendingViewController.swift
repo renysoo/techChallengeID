c3d3a2a15f56c92634d3483bfc0117b885717225
Unknown <rml7@academy.cin.ufpe.br>
2021-06-26 20:14:12 -0300

    search query implemented

--- techChallengeApp/techChallengeApp/App/Trending/TrendingViewController.swift
+++ techChallengeApp/techChallengeApp/App/Trending/TrendingViewController.swift
@@ -12,29 +12,47 @@ class TrendingViewController: UIViewController {
 
     
     private var moviesList: [Movie]?
+    
+    let network = NetworkManager()
 
     private let screen = TrendingScreen()
     
+    var searchTask: DispatchWorkItem?
+
     override func loadView() {
         self.view = screen
         screen.delegate = self
     }
     
+    fileprivate func fetchTrendingMovies() {
+        network.fetchTrending { [weak self] (movies) in
+            self?.moviesList = movies
+            DispatchQueue.main.async {
+                self?.screen.moviesTableView.reloadData()
+                
+            }
+        }
+    }
+    
+    fileprivate func searchMovieBy(searchTerm: String) {
+        network.fetchSearch(term: searchTerm) { [weak self] (movies) in
+            self?.moviesList = movies
+             DispatchQueue.main.async {
+                self?.screen.moviesTableView.reloadData()
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        screen.searchBar.delegate = self
         screen.moviesTableView.delegate = self
         screen.moviesTableView.dataSource = self
         screen.moviesTableView.register(MovieCustomCell.self, forCellReuseIdentifier: "moviesCell")
         self.navigationController?.setNavigationBarHidden(true, animated: true)
 
-        NetworkManager().fetchTrending { [weak self] (movies) in
-             self?.moviesList = movies
-             DispatchQueue.main.async {
-                self?.screen.moviesTableView.reloadData()
-                
-            }
-        }
+        fetchTrendingMovies()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -48,7 +66,7 @@ class TrendingViewController: UIViewController {
     }
 }
 
-extension TrendingViewController: UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+extension TrendingViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         moviesList?.count ?? 0
@@ -62,7 +80,7 @@ extension TrendingViewController: UITableViewDelegate, UITableViewDataSource, UI
         cell.movieLabel.text = moviesList?[indexPath.row].title
         cell.movieLabel.contentMode = .bottomRight
         cell.yearLabel.text = String(moviesList?[indexPath.row].releaseDate.prefix(4) ?? "")
-        cell.movieImage.loadImageUsingCache(withUrl: "https://image.tmdb.org/t/p/original" +  ((moviesList?[indexPath.row].posterPath)!))
+        cell.movieImage.loadImageUsingCache(withUrl: "https://image.tmdb.org/t/p/original" +  ((moviesList?[indexPath.row].posterPath) ?? ""))
         return cell
         
     }
@@ -74,6 +92,34 @@ extension TrendingViewController: UITableViewDelegate, UITableViewDataSource, UI
 
     }
     
+   
+    
+}
+
+extension TrendingViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchMovieBy(searchTerm: searchBar.text ?? "")
+       }
+       
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        self.searchTask?.cancel()
+        
+        let task = DispatchWorkItem { [weak self] in
+            if searchText == ""{
+                self?.fetchTrendingMovies()
+            } else {
+                self?.searchMovieBy(searchTerm: searchText)
+            }
+        }
+        self.searchTask = task
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.75, execute: task)
+    }
+    
+    
+       func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+           fetchTrendingMovies()
+       }
 }
 
 extension TrendingViewController: TrendingScreenDelegate {
